2ea236b0d3ebf1b4c9d538a39825e24e407b68d4
kong-nike <sokkong800800@gmail.com>
2024-12-03 14:33:33 +0700

    Update exercise_2.cpp

--- W7 - STARTCODE/exercise_2.cpp
+++ W7 - STARTCODE/exercise_2.cpp
@@ -1,44 +1,47 @@
 #include <iostream>
-#include <random>  
 #include "BinaryTree.h"
 #include "Node.h"
 
-int generateRandomNumber(int lower, int upper) {
-    std::random_device rd; 
-    std::mt19937 gen(rd()); 
-    std::uniform_int_distribution<> dist(lower, upper);  
-    return dist(gen);  
+void insert(Node* root, int value){
+    if (value < root->data){
+        if (root->left == NULL){
+            root->left = new Node(value);
+        } else {
+            insert(root->left, value);
+        }
+    } else {
+        if (root->right == NULL) {
+            root->right = new Node(value);
+        } else {
+            insert(root->right, value);
+        }
+    }
 }
 
-int getTreeHeight(Node* node) {
-    if (node == nullptr) {
+int Height(Node* node){
+    if(node == NULL){
         return 0;
     }
-    int leftHeight = getTreeHeight(node->left);
-    int rightHeight = getTreeHeight(node->right);
+    int leftHeight = Height(node->left);
+    int rightHeight = Height(node->right);
     return std::max(leftHeight, rightHeight) + 1;
 }
 
 int main() {
-
-    int rootValue = generateRandomNumber(1, 100); 
-    BinaryTree tree(rootValue);
-
-    for (int i = 0; i < 50; ++i) {
-        int newValue = generateRandomNumber(1, 100); 
-        if (rand() % 2 == 0) {  
-            tree.insertLeft(newValue);
-        } else {
-            tree.insertRight(newValue);
-        }
+    srand(time(0));
+    BinaryTree tree(50);
+    std::cout << "Numbers Generated: ";
+    for (int i = 1; i <= 50; i++) {
+        int num = rand()%100;
+        std::cout << num << " ";
+        insert(tree.getroot(), num);
     }
-    
-    std::cout << "In-order Traversal: " << tree.inOrderTraverse() << std::endl;
-    std::cout << "Pre-order Traversal: " << tree.preOrderTraverse() << std::endl;
-    std::cout << "Post-order Traversal: " << tree.postOrderTraverse() << std::endl;
 
-    int height = getTreeHeight(tree.getroot());
-    std::cout << "Height of the tree: " << height << std::endl;
-
+    std::cout << "\n";
+    std::cout << "\nIn-Order Traversal: " << tree.inOrderTraverse() << "\n";
+    std::cout << "Pre-Order Traversal: " << tree.preOrderTraverse() << "\n";
+    std::cout << "Post-Order Traversal: " << tree.postOrderTraverse() << "\n";
+    int height = Height(tree.getroot());
+    std::cout << "Height of the Tree: " << height << "\n";
     return 0;
 }
